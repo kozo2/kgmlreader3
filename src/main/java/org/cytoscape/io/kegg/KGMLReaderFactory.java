c254c031af3fa7eb7499fa2dede47db976ab1471
Kozo Nishida <knishida@riken.jp>
2011-08-26 14:38:40 +0900

    added impl almost same with AbstractNetworkReaderFactory

--- src/main/java/org/cytoscape/io/kegg/KGMLReaderFactory.java
+++ src/main/java/org/cytoscape/io/kegg/KGMLReaderFactory.java
@@ -4,10 +4,27 @@ import java.io.InputStream;
 
 import org.cytoscape.io.CyFileFilter;
 import org.cytoscape.io.read.InputStreamTaskFactory;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.view.model.CyNetworkViewFactory;
 import org.cytoscape.work.TaskIterator;
 
 public class KGMLReaderFactory implements InputStreamTaskFactory {
 
+	private final CyFileFilter filter;
+	
+	protected final CyNetworkViewFactory cyNetworkViewFactory;
+	protected final CyNetworkFactory cyNetworFactory;
+	
+	protected InputStream inputStream;
+	protected String inputName;
+	
+	public KGMLReaderFactory(CyFileFilter filter, CyNetworkViewFactory cyNetworkViewFactory,
+			CyNetworkFactory cyNetworkFactory) {
+		this.filter = filter;
+		this.cyNetworkViewFactory = cyNetworkViewFactory;
+		this.cyNetworFactory = cyNetworkFactory;		
+	}
+	
 	@Override
 	public TaskIterator getTaskIterator() {
 		// TODO Auto-generated method stub
@@ -16,14 +33,15 @@ public class KGMLReaderFactory implements InputStreamTaskFactory {
 
 	@Override
 	public CyFileFilter getCyFileFilter() {
-		// TODO Auto-generated method stub
-		return null;
+		return filter;
 	}
 
 	@Override
-	public void setInputStream(InputStream is, String inputName) {
-		// TODO Auto-generated method stub
-		
+	public void setInputStream(InputStream is, String in) {
+		if (is == null)
+			throw new NullPointerException("Input stream is null");
+		inputStream = is;
+		inputName = in;
 	}
 
 }
